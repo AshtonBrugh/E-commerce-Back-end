5faaff164726842a04df8993a519ffe79aeac58a
Ashton B <ashtonbrugh@gmail.com>
2022-01-12 17:01:03 -0500

    downloading packages

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+const mysql = require('mysql2');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
